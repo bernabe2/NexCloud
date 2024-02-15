fcc59f96f4b22eb856dc6aa63f4a4ea6ae017109
Alexis Salazar <appletehuacan@Macbook-Air-de-Alexis.local>
2024-02-15 14:01:25 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,34 @@
-import "./App.css"
-import HomePage from "./home/HomePage"
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
-import SinglePage from "./components/watch/SinglePage"
-import Header from "./components/header/Header"
-import Footer from "./components/footer/Footer"
-import Pages from "./components/Pages"
-import SeriesPage from "./components/SeriesPage"
-import MoviesPage from "./components/MoviesPage"
-import Pricing from "./components/Pricing"
-import Contact from "./components/Contact"
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./components/Redux/store"; 
+
+import "./App.css";
+import HomePage from "./home/HomePage";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import SeriesPage from "./components/SeriesPage";
+import MoviesPage from "./components/MoviesPage";
+import Pages from "./components/Pages";
+import Pricing from "./components/Pricing";
+import Contact from "./components/Contact";
 
 function App() {
   return (
-    <Router>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/series" component={SeriesPage} /> 
-        <Route path="/movies" component={MoviesPage} /> 
-        <Route path="/pages" component={Pages} />
-        <Route path="/pricing" component={Pricing} />
-        <Route path="/contact" component={Contact} />
-      </Switch>
-      <Footer />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/series" component={SeriesPage} />
+          <Route path="/movies" component={MoviesPage} />
+          <Route path="/pages" component={Pages} />
+          <Route path="/pricing" component={Pricing} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
+        <Footer />
+      </Router>
+    </Provider>
   );
 }
 
